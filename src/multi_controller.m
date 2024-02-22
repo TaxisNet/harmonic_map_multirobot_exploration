a9d0d24ed61488143b91313085459807149d875f
Taxis Blounas <taxisblounas@gmail.com>
2024-02-22 14:04:17 +0200

    Update multi_controller.m

--- src/multi_controller.m
+++ src/multi_controller.m
@@ -9,33 +9,34 @@ rehash toolboxcache
 
 rosinit
 
-global hm_cell q_front_cell robot_pos_cell namespace K_ang K_lin 
-hm_cell = {HarmonicMap(); HarmonicMap()};
+global hm_cell q_front_cell robot_pos_cell namespace boundary_info_sub velocity_pub
+global K_ang K_lin 
 K_ang = 0.4;
 K_lin = 0.05;
 
-namespace ={ 'tb3_0', 'tb3_1'};
-robot_pos_cell= cell(1,2);
-q_front_cell = cell(1,2);
-%ROS
-% node = ros.Node('/matlab_node');
-% sub = ros.Subscriber(node,'boundary_info','boundary_compute/boundary_info', @callback , DataFormat='struct');
-% vel_pub = ros.Publisher(node, '/cmd_vel','geometry_msgs/Twist');
+N = 2;
+hm_cell = cell(1,N);
+namespace = cell(1,N);
+robot_pos_cell = cell(1,N);
+q_front_cell = cell(1,N);
 
-boundary_info_sub_1 = rossubscriber(strcat(namespace{1},'/boundary_info'),'boundary_compute/boundary_info', @callback_1 , DataFormat='struct');
-velocity_pub_1 = rospublisher(strcat(namespace{1},'/cmd_vel'),'geometry_msgs/Twist', DataFormat='struct');
+boundary_info_sub = cell(1,N);
+velocity_pub = cell(1,N);
 
-boundary_info_sub_2 = rossubscriber(strcat(namespace{2},'/boundary_info'),'boundary_compute/boundary_info', @callback_2, DataFormat='struct');
-velocity_pub_2  = rospublisher(strcat(namespace{2},'/cmd_vel'),'geometry_msgs/Twist', DataFormat='struct');
+for k=1:N
+hm_cell{k} = HarmonicMap();
+namespace{k} = strcat("tb3_", string(k));
 
+boundary_info_sub{k} = rossubscriber(strcat(namespace{k},'/boundary_info'),'boundary_compute/boundary_info', {@callback, k}, DataFormat='struct');
+velocity_pub{k} = rospublisher(strcat(namespace{k},'/cmd_vel'),'geometry_msgs/Twist', DataFormat='struct');
+end
 
-velocity_pub = {velocity_pub_1, velocity_pub_2};
 
 tftree = rostf("DataFormat","struct");
-
 rate = rosrate(10);
+
 while(0)
-    parfor i=1:length(hm_cell)
+    parfor i=1:length(N)
         twistMsg = rosmessage(velocity_pub{i});
         if(~isempty(hm_cell{i}.frontiers_q) && ~isempty(q_front_cell{i})&& 0)
             robotPosMsg = getTransform(tftree, strcat( namespace{i}, '/map'), strcat( namespace{i}, '/base_footprint'));
@@ -71,10 +72,9 @@ while(0)
 end
 
 
-function callback_1(~,msg)
+function callback(~,msg, robot_num)
     global hm_cell q_front_cell robot_pos_cell
 
-    robot_num=1;
     if(~msg.CompFailed)
         [boundaries, isFree, ~] = parseBoundaries(msg);
         % check if order is counter clock wise.
@@ -112,51 +112,7 @@ function callback_1(~,msg)
         catch
             disp("Error finding nearest frontier")
             q_front_cell{robot_num} = hm_cell{robot_num}.frontiers_q(1,:)';
-        end
-        
-    end
-end
-
-function callback_2(~,msg)
-    global hm_cell q_front_cell robot_pos_cell
-
-    robot_num=2;
-    if(~msg.CompFailed)
-        [boundaries, isFree, ~] = parseBoundaries(msg);
-        % check if order is counter clock wise.
-        % Looks like the outter is and the inner aren't
-        % The order depends on the ouput of cv.findContours
-        % But documentation isn't very clear
-        % Check out:  https://answers.opencv.org/question/170874/cvfindcontours-orientation/
-        for i=1:length(boundaries)
-            try
-                if(~determinePointOrder(boundaries{i}))
-                    boundaries{i} = flip(boundaries{i},1);
-                    isFree{i} = flip(isFree{i},1);
-                end
-            catch ME
-                disp(ME.message)
-            end
-        end
-        
-        %calculate transform
-        tic
-        hm_cell{robot_num}.setBoundaries(boundaries,isFree);
-        toc
-        hm_cell{robot_num}.plotMap
-        
-        if(isempty(hm_cell{robot_num}.frontiers_q))
-            disp("Exporation Done!")
-            return
-        end
-        
-        try
-            q_front_cell{robot_num} = hm_cell{robot_num}.getNearestFrontier(robot_pos_cell{robot_num}, true);
-        catch
-            disp("Error finding nearest frontier")
-            q_front_cell{robot_num} = hm_cell{robot_num}.frontiers_q(1,:)';
-        end
-        
+        end        
     end
 end
 
@@ -222,6 +178,3 @@ function [linVel, angVel] = velocityController(quat, desired_vel)
     turningCoef = abs(delta_yaw)<(pi/3);
     linVel = K_lin* turningCoef*norm(desired_vel);
 end
-
-
-
